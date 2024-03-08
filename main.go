09ef8a12903dec43d84cd489b77b1f2ae7ced0e9
Pavel Syrovatskii <pavel1107syr@gmail.com>
2024-03-09 00:35:05 +0300

    fix main, finish v1

--- main.go
+++ main.go
@@ -3,16 +3,32 @@ package main
 import (
 	"flag"
 	"log"
-	"title-save-bot/clients/telegram"
+	tgClient "title-save-bot/clients/telegram"
+	eventconsumer "title-save-bot/consumer/event-consumer"
+	"title-save-bot/events/telegram"
+	"title-save-bot/storage/files"
 )
 
 const (
-	tgBotHost = "api.telegram.org"
+	tgBotHost   = "api.telegram.org"
+	storagePath = "storage"
+	batchSize   = 100
 )
 
 func main() {
 
-	_tgClient := telegram.New(tgBotHost, mustToken())
+	eventsProcessor := telegram.New(
+		tgClient.New(tgBotHost, mustToken()),
+		files.New(storagePath),
+	)
+
+	log.Print("service started")
+
+	consumer := eventconsumer.NewConsumer(eventsProcessor, eventsProcessor, batchSize)
+
+	if err := consumer.Start(); err != nil {
+		log.Fatal("service is stoped", err)
+	}
 
 }
 
